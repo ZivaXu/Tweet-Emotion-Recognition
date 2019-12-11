75407759a72f484047b03b487fe16bb414dd6e41
lindaYuelingLai <yl273@uw.edu>
2019-12-11 03:09:24 -0800

    completed all data fetching and processing code

--- main.py
+++ main.py
@@ -1,124 +1,143 @@
-### HCDE310-HW7
-### Linda Lai
-### The code can be run both locally on dev_appservery.py .
-### and Google's servers (at https://hcde310-hw7-linda.appspot.com)
-# import webapp2, os, urllib2, json, jinja2, logging, urllib
-#
-# JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-#     extensions=['jinja2.ext.autoescape'],
-#     autoescape=True)
+### HCDE 310 - Final Project: Tweet Emotion Recognition
+### authors: Linda Lai, Ziva Xu
+
+import webapp2, os, urllib2, json, jinja2, logging, urllib
+from ibm_watson import ToneAnalyzerV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson import ApiException
+
+import plotly
+import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
+
+import keys as keys     # file for api keys
+import twitter      # A Python wrapper around the Twitter API.
 
 
-### code from hw6
-### Utility functions you may want to use
-# def pretty(obj):
-#     return json.dumps(obj, sort_keys=True, indent=2)
-#
-# def safeGet(url):
-#     try:
-#         return urllib2.urlopen(url)
-#     except urllib2.URLError as e:
-#         logging.error("The server couldn't fulfill the request." )
-#         logging.error("Error code: ", e.code)
-#     except urllib2.URLError as e:
-#         logging.error("We failed to reach a server")
-#         logging.error("Reason: ", e.reason)
-#     return None
+JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+                                       extensions=['jinja2.ext.autoescape'],
+                                       autoescape=True)
 
-### flickrREST
-# import flickr_key as flickr_key
-# def flickrREST(baseurl = 'https://api.flickr.com/services/rest/',
-#     method = 'flickr.photos.search',
-#     api_key = flickr_key.key,
-#     format = 'json',
-#     params={},
-#     printurl = False
-#     ):
-#     params['method'] = method
-#     params['api_key'] = api_key
-#     params['format'] = format
-#     if format == "json": params["nojsoncallback"]=True
-#     url = baseurl + "?" + urllib.urlencode(params)
-#     if printurl:
-#         logging.info(url)
-#     return safeGet(url)
+# Utility functions you may want to use
+def pretty(obj):
+    return json.dumps(obj, sort_keys=True, indent=2)
 
-## getPhotoIDs() ###
-# getPhotoIDs() uses the flickr API to search
-# for photos with a given tag, and return a list of photo IDs for the
-# corresponding photos. Use a list comprehension to generate the list.
-#
-# Inputs:
-#   tag: a tag to search for
-#   n: the number of search results per page (default value should be 100)
-#
-# Returns: a list of (at most) n photo ids, or None if an error occured
-
-# def getPhotoIDs(tags="Seattle",n=100):
-#     resp = flickrREST(params={"tags":tags,"per_page":n})
-#     if resp is not None:
-#         photosdict = json.loads(resp.read())['photos']
-#         if photosdict is not None:
-#             if 'photo' in photosdict and len(photosdict['photo']) > 0:
-#                 return [photo['id'] for photo in photosdict['photo']]
-#     return None
-
-## getPhotoInfo() ##
-# getPhotoInfo() uses the flickr API to
-# get information about a particular photo id. The information should be
-# returned as a dictionary Hint: use flickrREST and the flickr API method
-# flickr.photos.getInfo, documented at
-# http://www.flickr.com/services/api/flickr.photos.getInfo.html
-#
-# Inputs:
-#   photoID: the id of the photo you to get information about
-#
-# Returns: a dictionary with photo info, or None if an error occurred
-
-# def getPhotoInfo(photoID):
-#      resp = flickrREST(method="flickr.photos.getInfo",params={"photo_id":photoID})
-#      if resp is not None:
-#          return json.load(resp)['photo']
-#      else:
-#         return None
+def safeGet(url):  # not currently being used
+    try:
+        return urllib2.urlopen(url)
+    except urllib2.URLError as e:
+        logging.error("The server couldn't fulfill the request.")
+        logging.error("Error code: ", e.code)
+    except urllib2.URLError as e:
+        logging.error("We failed to reach a server")
+        logging.error("Reason: ", e.reason)
+    return None
 
 
-## a class called Photo to represent Flickr photos ##
-# class Photo():
-#     def __init__(self,pd):
-#         self.title=pd['title']['_content']
-#         self.author=pd['owner']['username']
-#         self.userid = pd['owner']['nsid']
-#         self.tags = [tag["_content"] for tag in pd['tags']['tag']]
-#         self.numViews = int(pd['views'])
-#         self.commentcount = int(pd['comments']['_content'])
-#         self.url = pd['urls']['url'][0]['_content']
-#         self.thumbnailURL = self.makePhotoURL(pd)
-#
-#     def makePhotoURL(self,pd,size="q"):
-#         ## get a photo url, following documentation at https://www.flickr.com/services/api/misc.urls.html
-#         url = "https://farm%s.staticflickr.com/%s/%s_%s_%s.jpg"%(pd['farm'],pd['server'],pd['id'],pd['secret'],size)
-#         return url
-#
-#     def __str__(self):
-#         return "~~~ %s ~~~\nauthor: %s\nnumber of tags: %d\nviews: %d\ncomments: %d"%(self.title ,self.author ,len(self.tags),self.numViews,self.commentcount)
+
+# fetch tweet data from twitter api
+# only retrieves Tweets that have been sent in the last 7 days
+# Returns a list of dictionaries
+def tweets_search(ACCESS_TOKEN = keys.ACCESS_TOKEN,
+    ACCESS_SECRET = keys.ACCESS_SECRET,
+    CONSUMER_KEY = keys.CONSUMER_KEY,
+    CONSUMER_SECRET = keys.CONSUMER_SECRET,
+    params = {},
+    q = '#UW',
+    lang = 'en',   # by default, only retrieve tweets in English
+    result_type = 'recent',
+    count = '100'):
+    params['q'] = q
+    params['result_type'] = result_type
+    params['count'] = count
+    params['lang'] = lang
+    query = urllib.urlencode(params)
+    api = twitter.Api(consumer_key=CONSUMER_KEY,
+        consumer_secret=CONSUMER_SECRET,
+        access_token_key=ACCESS_TOKEN,
+        access_token_secret=ACCESS_SECRET)
+    logging.info("query: ", query)
+    results = api.GetSearch(raw_query=query)
+    return results
+
+
+# combine all the tweets text into one string
+# Returns a string (that will be processed with function tone_analyzer)
+def tweets_combine(tweets):
+    combined_string = tweets
+    for each_tweet in tweets:
+        tweet_text = each_tweet.text
+        combined_string = combined_string + tweet_text + ".\n"
+    return combined_string
+
+
+
+# analyze the tone of the given text
+watson_api = keys.WATSON_API
+
+def tone_analyzer(tone_input,
+    url="https://gateway.watsonplatform.net/tone-analyzer/api",
+    content_type="application/json",
+    sentences=False
+    ):
+    authenticator = IAMAuthenticator(watson_api)
+    tone_analyzer = ToneAnalyzerV3(version='2017-09-21', authenticator=authenticator)
+    tone_analyzer.set_service_url(url)
+    text = tone_input
+    try:
+        return tone_analyzer.tone({'text': text}, content_type=content_type, sentences=sentences).get_result()
+    except ApiException as ex:
+        if hasattr(ex, "code"):
+            logging.error("The server couldn't fulfill the request.")
+            logging.error("Error code: ", ex.code)
+        elif hasattr(ex, 'reason'):
+            logging.error("We failed to reach a server")
+            logging.error("Reason: ", ex.message)
+        return None
+
+
+def create_plot_one(): # this isn't final, just a sample call of the bar graph
+    N = 40
+    x = np.linspace(0, 1, N)
+    y = np.random.randn(N)
+    df = pd.DataFrame({'x': x, 'y': y}) # creating a sample dataframe
+    data = [
+        go.Bar(
+            x=df['x'], # assign x as the dataframe column 'x'
+            y=df['y']
+        )
+    ]
+    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
+
+
+# def create_plot_two():
 
 
 ###############
-# class MainHandler(webapp2.RequestHandler):
-#     def genpage(self, tag="northcascades"):
-#         photos = [Photo(getPhotoInfo(pid)) for pid in getPhotoIDs(tag)]
-#         tvals = {'results': {}, 'tag': tag}
-#         tvals['results']['views'] = sorted(photos, key=lambda x: x.numViews, reverse=True)[:3]
-#
-#         template = JINJA_ENVIRONMENT.get_template('hw6flickrtemplate-sol.html')
-#         self.response.write(template.render(tvals))
-#
-#     def get(self):
-#         self.genpage()
-#
-#     def post(self):
-#         tag = self.request.get('tag')
-#         self.genpage(tag)
-#
-# application = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
+class MainHandler(webapp2.RequestHandler):
+    def genpage(self, tag="#UW"):
+        if tag[0] != "#":
+            tag = "#" + tag     # make sure its a valid tag
+        tweets_data = tweets_search(q=tag)
+        combined_tweets = tweets_combine(tweets_data)
+        tone_analyzer(combined_tweets)
+        # not sure what type of data the function tone_analyzer returns
+
+        plot_one = create_plot_one()
+        plot_two = create_plot_two()
+
+        tvals = {'tag': tag, 'plot_one': plot_one, 'plot_two': plot_two}
+        template = JINJA_ENVIRONMENT.get_template('template.html')
+        self.response.write(template.render(tvals))
+
+    def get(self):
+        self.genpage()
+
+    def post(self):
+        tag = self.request.get('tag')
+        self.genpage(tag)
+
+
+application = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
